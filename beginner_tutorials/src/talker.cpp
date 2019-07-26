0677486deb9cfd3dc0053b88e04e0e7a3f08235b
Your Name <you@example.com>
2019-07-26 18:50:28 +0200

    new commit

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -44,7 +44,7 @@
       * than we can send them, the number here specifies how many messages to
       * buffer up before throwing some away.
       */
-     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("joint_states", 1000);
    
      ros::Rate loop_rate(10);
    
